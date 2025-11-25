f4050214bdc85af9b7a8dc9822806c5bbb19e14e
James <jamesandrewpike@gmail.com>
2025-11-25 16:29:44 -0500

    commit

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -18,14 +18,14 @@ export default component$(() => {
         <img
           src="/images/logo22.svg"
           alt="earthen vessels Logo"
-          class="px-16 -mt-2 h-48 mx-auto md:hidden"
+          class="px-16 -mt-4 h-48 mx-auto md:hidden"
         />
 
         {/* Left Column (Desktop) / Text Content (Mobile) */}
-        <div class="relative z-10 order-1 flex items-center justify-center px-4 pt-4 pb-10 md:pl-8 md:py-12 md:order-1">
+        <div class="relative z-10 order-1 flex items-center justify-center px-4 pt-3 pb-10 md:pl-8 md:py-12 md:order-1">
           <div class="text-center md:text-left px-2">
             {/* Headline (Desktop only) */}
-            <h1 class=" md:block text-5.5xl md:text-7xl font-bold tracking-tight md:mb-4 mb-8 md:-mt-0 -mt-10 ">
+            <h1 class=" md:block text-5.5xl md:text-7xl font-bold tracking-tight md:mb-4 mb-6 md:-mt-0 -mt-10 ">
               <span class="bg-gradient-to-r xdxd from-secondary-800 via-tertiary-500 to-secondary-800  bg-clip-text text-transparent">
                 earthen vessels
               </span>
